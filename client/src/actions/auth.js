1ed0d5555a48c34ed8f5fdc4d8a31b48f21eec42
Sergey <Prostov>
2020-05-24 21:59:21 +0300

    Check user profile, clearprofile aftre logout, added spinner

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,14 @@
 import axios from 'axios'
-import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT } from './types'
+import {
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  CLEAR_PROFILE
+} from './types'
 import { setAlert } from './alert'
 import setAuthToken from '../utils/setAuthToken'
 
@@ -86,5 +95,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout. Clear Profile
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE })
   dispatch({ type: LOGOUT })
 }
